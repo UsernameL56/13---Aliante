ae328079985f1e8a8d9703a5d070b4be892dbfdc
Lucas Corsi <corsi.lucasgabriel.studente@itispaleocapa.it>
2023-12-31 13:57:36 +0100

    classe Cerchione

---
+++ Classi/Cerchione.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classi
+{
+    class Cerchione : Componente
+    {
+        // attributi
+        private double _pollici;
+        private string _materiale;
+        private double _prezzo;
+
+        // metodi accessor
+        public double Pollici
+        {
+            get { return _pollici; }
+            set { _pollici = value; }
+        }
+        public string Materiale
+        {
+            get { return _materiale; }
+            set { _materiale = value; }
+        }
+        public double Prezzo
+        {
+            get { return _prezzo; }
+            set { _prezzo = value; }
+        }
+
+        // costruttore
+        public Cerchione(double _pollici, string _materiale, double _prezzo)
+        {
+            Pollici = _pollici;
+            Materiale = _materiale;
+            Prezzo = _prezzo;
+        }
+
+        // metodi
+        public override double Costo()
+        {
+            return Prezzo;
+        }
+
+        public override string Descrizione()
+        {
+            return $"Cerchione - Pollici: {Pollici}, Materiale: {Materiale}, Prezzo: {Prezzo}";
+        }
+    }
+}
