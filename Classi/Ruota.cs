467c0e5db149e3ba6e4279ed546e6e65aaf49bb4
Lucas Corsi <corsi.lucasgabriel.studente@itispaleocapa.it>
2023-12-31 15:26:17 +0100

    classe Ruota

---
+++ Classi/Ruota.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classi
+{
+    class Ruota : Componente
+    {
+        // attributi
+        private Cerchione _cerchione;
+        private Gomma _gomma;
+
+        // metodi accessor
+        public Cerchione Cerchione
+        {
+            get { return _cerchione; }
+            set { _cerchione = value; }
+        }
+        public Gomma Gomma
+        {
+            get { return _gomma; }
+            set { _gomma = value; }
+        }
+
+        // costruttore
+        public Ruota(Cerchione _cerchione, Gomma _gomma)
+        {
+            Cerchione = _cerchione;
+            Gomma = _gomma;
+        }
+
+        // metodi
+        public override double Costo()
+        {
+            return _cerchione.Costo() + _gomma.Costo();
+        }
+        public override string Descrizione()
+        {
+            return $"Ruota - {_cerchione.Descrizione()}, {_gomma.Descrizione()}";
+        }
+    }
+}
